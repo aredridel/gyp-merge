0f58dd27a906cfbdb1aa9179f6bc834610095769
Aria Stewart <aredridel@nbtsc.org>
2013-03-10 14:47:05 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    mergeDictionary: mergeDictionary,
+    mergeList: mergeList
+};
+
+function mergeDictionary(dest, src) {
+    var out = Object.create(dest);
+
+    if (typeof src == 'undefined') {
+        return out;
+    }
+
+    if (typeof src != 'object' || Array.isArray(src) || typeof dest != 'object' || Array.isArray(dest)) {
+        throw new TypeError("dest and src must both be objects");
+    }
+
+    for (var k in src) {
+        if (Array.isArray(src[k])) {
+            var suffix = k.substr(-1);
+            var k2 = (suffix != '?' && suffix != '=' && suffix != '+') ? k : k.substr(0, k.length - 1);
+            out[k2] = mergeList(out[k2], src[k], suffix);
+        } else if (typeof src[k] == 'object') {
+            out[k] = mergeDictionary(out[k], src[k]);
+        } else {
+            out[k] = src[k];
+        }
+    }
+
+    return out;
+}
+
+function mergeList(dest, src, suffix) {
+    if (!dest || suffix == '=') {
+        return src;
+    } else if (suffix == '?') {
+        return dest ? dest : src;
+    } else if (suffix == '+') {
+        return src.concat(dest);
+    } else {
+        return dest.concat(src);
+    }
+}
