570ffb5f8aa1ce4da0b3364c6974c2d7dc2c27b9
Aria Stewart <aredridel@nbtsc.org>
2013-03-10 17:32:42 -0400

    Add "singleton" handling when merging lists

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
-module.exports = {
-    mergeDictionary: mergeDictionary,
-    mergeList: mergeList
-};
+module.exports = function(config) {
+    if (!config) config = {
+        noSingletons: false
+    };
 
-function mergeDictionary(dest, src) {
-    var out = Object.create(dest);
+    function mergeDictionary(dest, src) {
+        var out = Object.create(dest);
 
-    if (typeof src == 'undefined') {
+        if (typeof src == 'undefined') {
+            return out;
+        }
+
+        if (typeof src != 'object' || Array.isArray(src) || typeof dest != 'object' || Array.isArray(dest)) {
+            throw new TypeError("dest and src must both be objects");
+        }
+
+        for (var k in src) {
+            if (Array.isArray(src[k])) {
+                var suffix = k.substr(-1);
+                var k2 = (suffix != '?' && suffix != '=' && suffix != '+') ? k : k.substr(0, k.length - 1);
+                out[k2] = mergeList(out[k2], src[k], suffix);
+            } else if (typeof src[k] == 'object') {
+                out[k] = mergeDictionary(out[k], src[k]);
+            } else {
+                out[k] = src[k];
+            }
+        }
+
         return out;
     }
 
-    if (typeof src != 'object' || Array.isArray(src) || typeof dest != 'object' || Array.isArray(dest)) {
-        throw new TypeError("dest and src must both be objects");
-    }
-
-    for (var k in src) {
-        if (Array.isArray(src[k])) {
-            var suffix = k.substr(-1);
-            var k2 = (suffix != '?' && suffix != '=' && suffix != '+') ? k : k.substr(0, k.length - 1);
-            out[k2] = mergeList(out[k2], src[k], suffix);
-        } else if (typeof src[k] == 'object') {
-            out[k] = mergeDictionary(out[k], src[k]);
+    function mergeList(dest, src, suffix) {
+        if (!dest || suffix == '=') {
+            return src;
+        } else if (suffix == '?') {
+            return dest ? dest : src;
+        } else if (suffix == '+') {
+            return concat(src, dest);
         } else {
-            out[k] = src[k];
+            return concat(dest, src);
         }
     }
 
-    return out;
-}
+    function concat(dest, src) {
+        if (!src) return dest;
+        if (!dest) return src;
 
-function mergeList(dest, src, suffix) {
-    if (!dest || suffix == '=') {
-        return src;
-    } else if (suffix == '?') {
-        return dest ? dest : src;
-    } else if (suffix == '+') {
-        return src.concat(dest);
-    } else {
-        return dest.concat(src);
+        if (config.noSingletons) {
+            return dest.concat(src);
+        } else {
+            var out = dest.slice(0);
+            for (var i = 0; i < src.length; i++) {
+                if (typeof src[i] != 'string' || src[i][0] == '-' || out.indexOf(src[i]) == -1) {
+                    out.push(src[i]);
+                }
+            }
+
+            return out;
+        }
     }
-}
+
+    return {
+        mergeDictionary: mergeDictionary,
+        mergeList: mergeList
+    };
+};
